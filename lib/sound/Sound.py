1867baad3f79508aee956995ae936c5618271317
legendmohe <legendmohe@qq.com>
2015-01-05 22:35:56 +0800

    modified:   lib/sound/Sound.py

--- lib/sound/Sound.py
+++ lib/sound/Sound.py
@@ -21,28 +21,7 @@ import urllib2
 from util.log import *
 
 
-def playwav(path):
-    import pyaudio  
-    import wave  
-
-    chunk = 1024  
-    f = wave.open(path, "rb")  
-    p = pyaudio.PyAudio()  
-    stream = p.open(format = p.get_format_from_width(f.getsampwidth()),  
-                            channels = f.getnchannels(),  
-                            rate = f.getframerate(),  
-                            output = True)  
-    data = f.readframes(chunk)  
-    while data != '':  
-        stream.write(data)  
-        data = f.readframes(chunk)  
-    stream.stop_stream()  
-    stream.close()  
-    p.terminate() 
-
-
 AUDIO_SERVER_ADDRESS = None
-
 
 def get_play_request_url(path, inqueue=False, loop=-1):
     global AUDIO_SERVER_ADDRESS
