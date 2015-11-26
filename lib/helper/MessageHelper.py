4791a9aacc3f67c654dd82522bebf15cb29bc4de
Xinyu He <legendmohe@foxmail.com>
2015-11-26 16:23:54 +0800

    Update MessageHelper.py

--- lib/helper/MessageHelper.py
+++ lib/helper/MessageHelper.py
@@ -29,20 +29,18 @@ from util.thread import TimerThread
 
 
 class MessageHelper(object):
-
-    LOCAL_SCAN_PORT = 9002
-    LOCAL_HEARTBEAT_RATE = 2
+    
+    LOCAL_HEARTBEAT_RATE = 5
     MESSAGE_DB = "./data/msg.pcl"
 
-    def __init__(self, pub_address, cmd_address):
+    def __init__(self, pub_address, hb_port):
         self.pub_address = pub_address
-        self.cmd_address = cmd_address
+        self.heartbeat_port = hb_port
         self._data_lock = threading.Lock()
         self._msg_lock = threading.Lock()
         self._msg_queue = Queue()
 
         self._init_data()
-        self._init_subscriber()
         self._init_pub_heartbeat()
         self._init_worker()
 
@@ -50,10 +48,6 @@ class MessageHelper(object):
         self._load_data()
         if 'seq' not in self._context:
             self._context['seq'] = 1
-
-    def _init_subscriber(self):
-        self._backup_dict = {}
-        self._subscribers = set()
 
     def _load_data(self):
         self._context = {}
@@ -74,20 +68,6 @@ class MessageHelper(object):
                 ERROR(e)
                 ERROR("invaild MessageHelp data path:%s", MessageHelper.MESSAGE_DB)
 
-    def add_subscribers(self, new_sub):
-        if new_sub is Null or len(new_sub) == 0:
-            ERROR("invaild subscriber.")
-            return False
-        self._subscribers.add(new_sub)
-        return True
-
-    def remove_subscribers(self, old_sub):
-        if old_sub is Null or len(old_sub) == 0:
-            ERROR("invaild subscriber.")
-            return False
-        self._subscribers.remove(old_sub)
-        return True
-
     def _msg_worker(self):
         context = zmq.Context()
         publisher = self.pub_address
@@ -103,15 +83,6 @@ class MessageHelper(object):
             self._pub_sock.send_string(msg_string)
             time.sleep(0.3)
 
-    def _cmd_worker(self):
-        context = zmq.Context()
-        socket = context.socket(zmq.REP)
-        socket.bind(self.cmd_address)
-        while True:
-            cmd = socket.recv_string()
-            res = self._handle_cmd(cmd)
-            socket.send_string(res)
-
     def _put_msg(self, msg):
         self._msg_queue.put(msg)
 
@@ -121,32 +92,6 @@ class MessageHelper(object):
                                 ) # block!
         self._msg_queue.task_done()
         return msg
-
-    def _handle_cmd(self, cmd):
-        try:
-            cmd_object = json.loads(cmd)
-            cmd_type = cmd_object["type"]
-            if cmd_type == "load":
-                cmd_from = cmd_object["from"]
-                cmd_to = cmd_object["to"]
-                msgs = self.msg_for_range(cmd_from, cmd_to)
-                res_string = json.dumps({"res": msgs})
-            elif cmd_type == "login":
-                user_id = cmd_object["id"]
-                self.add_subscribers(user_id)
-                res_string = json.dumps({"res": "ok", "maxseq": self._context['seq']})
-            elif cmd_type == "logout":
-                user_id = cmd_object["id"]
-                self.remove_subscribers(user_id)
-                res_string = json.dumps({"res": "ok"})
-            elif cmd_type == "done":
-                seq = cmd_object["seq"]
-                self.done_msg(seq)
-                res_string = json.dumps({"res": "ok"})
-        except Exception, e:
-            ERROR(e)
-            res_string = json.dumps({"res": "error"})
-        return res_string
 
     def _init_pub_heartbeat(self):
         def heartbeat():
@@ -158,7 +103,7 @@ class MessageHelper(object):
         local_sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, True)
         local_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
         def local_heartbeat():
-            address = ("255.255.255.255", MessageHelper.LOCAL_SCAN_PORT)
+            address = ("255.255.255.255", self.heartbeat_port)
             local_sock.sendto("ok", address)
 
         local_heartbeat_thread = TimerThread(
@@ -168,54 +113,27 @@ class MessageHelper(object):
         local_heartbeat_thread.start()
 
     def _init_worker(self):
-        self._cmd_thread = threading.Thread(target=self._cmd_worker)
-        self._cmd_thread.daemon = True
-        self._cmd_thread.start()
-
         self._msg_thread = threading.Thread(target=self._msg_worker)
         self._msg_thread.daemon = True
         self._msg_thread.start()
 
     def publish_msg(self, sub_id, msg, cmd_type="normal"):
+        msg_dict = {
+                    "type": cmd_type,
+                    "msg": msg,
+                    "ts": "%d" % int(time.time()),
+                   }
         with self._msg_lock:
-            msg_dict = {
-                        "type": cmd_type,
-                        "msg": msg,
-                        "ts": "%d" % int(time.time()),
-                       }
-
             if cmd_type != "heartbeat":
                 self._context['seq'] += 1
                 msg_dict["seq"] = self._context['seq']
-                msg_dict["maxseq"] = self._context['seq']
                 msg_string = json.dumps(msg_dict)
-                if len(self._subscribers) > 0:
-                    self._backup_dict[self._context['seq']] = (
-                                            msg_string, len(self._subscribers))
+                DEBUG("push msg:%s-%s-%s" % (self._context['seq'], cmd_type, msg))
             else:
                 msg_dict["seq"] = -1
-                msg_dict["maxseq"] = self._context['seq']
                 msg_string = json.dumps(msg_dict)
-            DEBUG("push msg:%s-%s-%s" % (self._context['seq'], cmd_type, msg))
             self._put_msg(msg_string)
             self._save_data()
-
-    def done_msg(self, seq):
-        if seq in self._backup_dict:
-            item = self._backup_dict[seq]
-            item[1] -= 1
-            if item[1] == 0:
-                del self._backup_dict[seq]
-        else:
-            WARN("no such seq:" + str(seq))
-
-    def msg_for_range(self, begin_seq, end_seq):
-        msgs = []
-        for seq in range(begin_seq, end_seq + 1):
-            if seq in self._backup_dict:
-                msg, _ = self._backup_dict[seq]
-                msg.append(msg)
-        return msgs
 
     def cur_seq(self):
         return self._context['seq']
