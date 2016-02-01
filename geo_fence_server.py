6295eb7e3cb2f63697bcbb0dc2ed098f225e04c7
Xinyu He <legendmohe@foxmail.com>
2016-02-01 23:21:35 +0800

    Update geo_fence_server.py

--- geo_fence_server.py
+++ geo_fence_server.py
@@ -4,38 +4,54 @@
 import json
 import threading
 import Queue
+import collections
 import time
 import random
+
 
 # ----------------------Util
 
 def enum(**enums):
     return type('Enum', (), enums)
 
+
 # ---------------------- Data
 
 Event = enum(ENTER=0, LEAVE=1, STAY=2)
 
+
 class GeoResolver:
-    def __init__(self, devices):
+    def __init__(self, devices, area):
         self._dev = {}
-        for name, device in devices.items(): 
+        self._area = area
+        for name, device in devices.items():
             self._dev[name] = device
 
     def resolve(self, target_name):
         print "processing loc for %s" % target_name
-        print item for item in self._dev.items
+        dev = self._dev.get(target_name, None)
+        if dev is not None:
+            intervals = []
+            cur_loc = dev.loc_queue[-1]
+            for area_name, area in self._area.items():
+                distance = self.cal_distance(cur_loc.lat, cur_loc.lan, area.lat, area.lan)
 
     def cal_distance(self, lat1, lan1, lat2, lan2):
-        pass
+        return 1.0
 
     def notify_state(name, event_type):
         pass
 
+    def dump(self):
+        for name, item in self._dev.items():
+            print name, [(loc.lat, loc.lan) for loc in list(item.loc_queue)]
+
+
 class Device:
     def __init__(self, name):
-        self.loc_queue = Queue.Queue(maxsize=15)
+        self.loc_queue = collections.deque(maxlen=5)
         self.name = name
+
 
 class Location:
     def __init__(self, device, lat, lan):
@@ -43,21 +59,30 @@ class Location:
         self.lan = lan
         self.device = device
 
+
+class Area:
+    def __init__(self, name, lat, lan):
+        self.lat = lat
+        self.lan = lan
+        self.name = name
+
+
 # ---------------------- Global
 
-g_loc_queue = Queue.Queue(maxsize=50)
+g_loc_queue = Queue.Queue(maxsize=25)
 g_devices = {}
-g_areas = {}
+g_area = {}
 g_resolver = None
+
 
 # ----------------------- Logic
 
 def fetch_loc_worker(device, loc_queue):
     print "%s worker thread start." % device.name
     while True:
-        rand = random.randint(3,10)
+        rand = random.randint(3, 10)
 
-        device.loc_queue.put(Location(device, rand, rand))
+        device.loc_queue.append(Location(device, rand, rand))
         loc_queue.put(device.name)
         loc_queue.task_done()
 
@@ -66,8 +91,10 @@ def fetch_loc_worker(device, loc_queue):
 
     print "%s worker thread stop." % device.name
 
+
 def init():
     load_data_from_conf("geo_conf.json")
+
 
 def load_data_from_conf(path):
     print "load conf:", path
@@ -79,17 +106,23 @@ def load_data_from_conf(path):
         print "load device:", name
         g_devices[name] = Device(name)
 
+    area = conf["area"]
+    for name, item in area.items():
+        print "load area:", name
+        g_area[name] = Area(name, item["lat"], item["lan"])
+
+
 def start():
     for name, device in g_devices.items():
         fetch_t = threading.Thread(
-                        target=fetch_loc_worker,
-                        args=(device, g_loc_queue)
-                        )
+            target=fetch_loc_worker,
+            args=(device, g_loc_queue)
+        )
         fetch_t.daemon = True
         fetch_t.start()
 
     try:
-        g_resolver = GeoResolver(g_devices)
+        g_resolver = GeoResolver(g_devices, g_area)
         while True:
             target_name = g_loc_queue.get()
             g_resolver.resolve(target_name)
@@ -98,9 +131,11 @@ def start():
 
     print "main thread exit."
 
+
 def main():
     init()
     start()
+
 
 if __name__ == "__main__":
     main()
